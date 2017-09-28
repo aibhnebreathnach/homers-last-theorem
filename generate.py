29ee77f8a662f998e1f3ca76df602252287648c4
Aibhne Breathnach <aibhnebreathnach@gmail.com>
2017-09-28 10:49:25 +0100

    Reformatted generate.py
    Reformatted generate.py to return unique tuples for a and b

---
+++ generate.py
@@ -0,0 +1,36 @@
+'''
+Script to generate data to feed fermat.py
+
+upper_bounds = array of upper bounds for numbers to test (1 -> upper_bound) for each [a/b, n]
+
+Intended to use at the start of every fermat.py script to save writing to file.
+But may have to write to file if numbers get to big to hold in memory
+'''
+import sys, itertools
+
+upper_bounds = sys.argv[1:]
+
+def generate(upper_bounds):
+	a_b_bound = int(upper_bounds[0])
+	n_bound = int(upper_bounds[1])
+
+	a_to_upper = range(1, a_b_bound + 1) # a -> upper bound
+	b_to_lower = a_to_upper[::-1] # lower bound <- b
+
+	# list of tuples of a and b
+	a_b_tuples = []
+
+	# Fill arrays 1 -> upper_bound
+	for index in range(len(a_to_upper)):
+		a_b_tuples.append((a_to_upper[index], b_to_lower[index]))
+
+	# Get the set of sorted tuples, removes duplicate permutations. ie (10, 1) and (1, 10)
+	a_b_tuples = set(tuple(sorted(l)) for l in a_b_tuples)
+
+	n_bound = range(3, n_bound + 1) # n > 2, therefore 3 -> upper bound
+
+	data_dict = {'a_b' : a_b_tuples, 'n' : n_bound}
+
+	return data_dict
+
+generate(upper_bounds)
